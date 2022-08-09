63eb8e0f4d142051d3dfdc2cf9fc44bd1a7b654d
Beth Allaway <beth_allaway@yahoo.com>
2022-08-09 20:36:32 +0100

    Add multiplication game mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,9 @@ function runGame(gameType) {
 
   if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
-  } else {
+  } else if (gameType === "multiply") {
+    displayMultiplyQuestion(num1, num2);
+  }else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
   }
@@ -66,7 +68,9 @@ function calculateCorrectAnswer() {
 
   if (operator === "+") {
       return [operand1 + operand2, "addition"];
-  } else {
+  } else if (operator === "x") {
+      return [operand1 * operand2, "multiply"];
+  }else {
       alert(`Unimplimented operator ${operator}`);
       throw `Unplimented operator ${operator}. Aborting!`;
   }
@@ -77,7 +81,7 @@ function calculateCorrectAnswer() {
  */
  function incrementScore() {
 
-    let oneScore = parseInt(document.getElementById("score").innerText);
+    let oldScore = parseInt(document.getElementById("score").innerText);
     document.getElementById("score").innerText = oldScore +1;
 }
 
@@ -98,6 +102,10 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion() {}
 
-function displayMultiplyQuestion() {}
+function displayMultiplyQuestion(operand1, operand2) {
 
+    document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "x";
+}
 function displayDivisionQuestion() {}
